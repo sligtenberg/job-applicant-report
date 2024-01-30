b9abfb5e5c1a5210811c43ff7d361f5284783e62
Stephen Ligtenberg <bountyhunter24@gmail.com>
2024-01-30 14:36:38 -0500

    data in db.json, json server installed, job applicant headers component built

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <thead>
+            <tr>
+                <th>Job</th>
+                <th>Applicant Name</th>
+                <th>Email Address</th>
+                <th>Website</th>
+                <th>Skills</th>
+                <th>Cover Letter Paragraph</th>
+            </tr>
+        </thead>
+    );
+}
+
+export default Header;
---
+++ src/MainPage.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+function MainPage() {
+    return (
+        <table class="job-applicants">
+            <Header />
+        </table>
+    );
+}
+
+export default MainPage;
